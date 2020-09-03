6f389774adda2b5979580b5c6ac8eda4701fa3fe
beqakd <bgoga16@freeuni.edu.ge>
2020-09-03 23:54:45 +0400

    fill website with proper texts

--- src/Components/HomeComponent/HomeComponent.js
+++ src/Components/HomeComponent/HomeComponent.js
@@ -24,7 +24,7 @@ export default class HomeComponent extends React.Component{
                     <img className={styles.backgroundImage} src={backgroundImage}/>
                     <div className={styles.backgroundLayer}>
                         <span className={styles.mainTitle}>Prelysium</span>
-                        <span className={styles.secondaryText}> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make</span>
+                        <span className={styles.secondaryText}> Is new AI breakthrough product, which aims to help companies fight coronavirus and its consequences, while following new guidelines structured by government.</span>
                     </div>
                     <div className={styles.title}>
                         <span className={styles.companyName}>Prelysium</span>
@@ -32,13 +32,13 @@ export default class HomeComponent extends React.Component{
                     </div>
                     <div className={styles.middleContainer}>
                         <div className={`${styles.leftTexts} ${styles.texts}`}>
-                            <TextItem title={"Image Classification"} text={"Filter and categorize images, monitor for inappropriate content, and automatically assign labels for all of your digital media."}/>
-                            <TextItem title={"Image Classification"} text={"Filter and categorize images, monitor for inappropriate content, and automatically assign labels for all of your digital media."}/>
+                            <TextItem title={"Protect Your Health"} text={"Safety first, each company has responsibility to stop Covid-19 from spreading around the world. Our product will help you to follow Government rules."}/>
+                            <TextItem title={"Safety Isn’t Expensive, It’s Priceless"} text={"In pandemic situations like this safety is priceless. Our product will help you to gain trust from customers and also, lead you to the better world."}/>
                         </div>
                         <Circle size={600}/>
                         <div className={`${styles.rightTexts} ${styles.texts}`}>
-                            <TextItem title={"Image Classification"} text={"Filter and categorize images, monitor for inappropriate content, and automatically assign labels for all of your digital media."}/>
-                            <TextItem title={"Image Classification"} text={"Filter and categorize images, monitor for inappropriate content, and automatically assign labels for all of your digital media."}/>
+                            <TextItem title={"Image/Video Classification"} text={"Filter and categorize images or videos, monitor for content, and automatically assign labels for all of your digital media."}/>
+                            <TextItem title={"Fine-Grained Machine Vision Recognition"} text={"Make things easier for customers and for your company. Marketplace through our product and gain newest AI real-time recognition software."}/>
                         </div>
                     </div>
 
